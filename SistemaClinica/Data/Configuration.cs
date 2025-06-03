15e3d8d217d42c62dbefa2a373bc0bd27f00e701
NoahEritier <eritiernoah@gmail.com>
2025-06-03 14:28:10 -0300

    conexion a base de datos. Cambiar el string de conexion segun corresponda y añadir el archivo al ignore

---
+++ SistemaClinica/Data/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaClinica.Data
+{
+    public static class Configuration
+    {
+        public static string ConnectionString => "server=localhost;database=sistemaclinica;user=root;password=4Y2y3p4e!";
+    }
+}
---
+++ SistemaClinica/Data/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SistemaClinica.Data
+{
+    public static class DatabaseConnection
+    {
+        public static MySqlConnection GetConnection()
+        {
+            var connection = new MySqlConnection(Configuration.ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
